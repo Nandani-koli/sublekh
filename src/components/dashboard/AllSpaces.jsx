731ef86a64a30ab80b2c1d932d4c3c110a997eb6
Nandani-koli <70843941+Nandani-koli@users.noreply.github.com>
2026-01-24 19:11:16 +0530

    Update AllSpaces.jsx

--- src/components/dashboard/AllSpaces.jsx
+++ src/components/dashboard/AllSpaces.jsx
@@ -69,6 +69,10 @@ const AllSpaces = async() => {
                 <Link href={`spacereviews/${space._id}`}>
                 <EyeIcon className="h-6 w-6 text-gray-500 hover:text-gray-700 cursor-pointer" />
                 </Link>
+
+                <Link href={`editspace/${space._id}`}>
+                <PencilIcon className="h-6 w-6 text-gray-500 hover:text-gray-700 cursor-pointer" />
+                </Link>
               </div>
             </div>
           ))}
